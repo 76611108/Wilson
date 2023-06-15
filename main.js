95a591d780c07aa644ca0ff52ef06709c9fb931d
76611108 <133533973+76611108@users.noreply.github.com>
2023-06-16 00:34:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,28 +2,17 @@
   File: main.js
   Description: JavaScript code for the website's functionality.
   Author: Remi Fernandez
-
 */
-
-
-<meta charset="UTF-8"/>
-
-<meta name="keywords" content="Wilson's Kitchen, organic,food"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 document.addEventListener("DOMContentLoaded", function() {
   var slider = document.getElementById("myrange");
   var value = document.getElementById("value");
 
-
   value.innerHTML = slider.value;
 
-  
   slider.addEventListener("input", function() {
     value.innerHTML = this.value;
   });
-});
-
 
   const menuSelect = document.getElementById('menu-select');
   const menuItems = document.querySelectorAll('.menu-item');
@@ -39,4 +28,5 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+});
 
